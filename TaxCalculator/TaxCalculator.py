274c34dec5901c2bd1b78173b2438b596a305f4d
codey-bower <143823683+codey-bower@users.noreply.github.com>
2023-09-01 16:20:50 +0000

    variable name change

--- TaxCalculator/TaxCalculator.py
+++ TaxCalculator/TaxCalculator.py
@@ -1,4 +1,4 @@
-principal = 200
-tax = 0.06
-amountDue = principal + (principal*tax)
-print("Amount Due: " + str(amountDue))
+PRINCIPAL = 200
+TAX = 0.06
+AMOUNT_DUE = PRINCIPAL + (PRINCIPAL*TAX)
+print("Amount Due: " + str(AMOUNT_DUE))
